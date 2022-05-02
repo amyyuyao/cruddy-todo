3cb408bdf7c4549bbbcc1cfd915717156fb5c685
Joy <parkerjn90@gmail.com>
2022-05-02 19:17:25 -0400

    continue working on getNextUniqueId

--- datastore/counter.js
+++ datastore/counter.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const sprintf = require('sprintf-js').sprintf;
+// test
+const counterTest = require('../test/counterTest.txt');
 
 var counter = 0;
 
@@ -38,13 +40,29 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
+// pass in callback from index???
+exports.getNextUniqueId = (callback) => {
+  // call readCounter, pass in a callback
+  readCounter((err, num) => {
+    // return numberfied string
+    counter = num;
+  })
+    // callback should take in two values, error? and a stringified number
+      // stringified number comes from saved file's text -- not quite surehow to get it
+      // set counter in this file to numberfied
+
+  writeCounter(counter + 1, (null, str) => {
+    fs.writeFile(counterFile, str);
+  });
+
   return zeroPaddedNumber(counter);
 };
 
+//fs.existsSync()
 
 
 // Configuration -- DO NOT MODIFY //////////////////////////////////////////////
 
 exports.counterFile = path.join(__dirname, 'counter.txt');
+// not quite understanding path.join? what is it doing?
+// /Users/parkerjn90/HackReactor/Sprints/rfe2204-cruddy-todo/datastore + /counter.txt
